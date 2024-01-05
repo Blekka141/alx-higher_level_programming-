d99052d0d7c8dc094339c84ff91c3bc4067c72a7
Keegan <keeganjoubert@protonmail.com>
2024-01-04 16:14:25 -0800

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-
-args = argv[1:]
-result = sum(int(arg) for arg in args)
-
-print(result)
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
