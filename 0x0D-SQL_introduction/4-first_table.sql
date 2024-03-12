0d2eabbe2b906312f4ff4e33510ce9e532a66c98
Keegan <keeganjoubert@protonmail.com>
2024-03-12 15:38:22 -0700

    SQL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
 -- This creates a table with the values of ID and NAME.
+CREATE TABLE IF NOT EXISTS first_table ('id' INT, 'name' VARCHAR(256));
+
