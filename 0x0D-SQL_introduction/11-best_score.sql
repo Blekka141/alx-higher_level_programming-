53efc96f681f11d26943803365f7e05adf1af0f6
Keegan <keeganjoubert@protonmail.com>
2024-03-12 15:27:35 -0700

    SQL

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `score` >= 10
-ORDER BY `score` DESC; 
+-- List all records with a score >= 10 in second_table, ordered by score
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
