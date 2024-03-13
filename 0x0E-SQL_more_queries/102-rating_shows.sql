4f23bff6c8b0e1f1c5b9833863071d74aedd8451
Keegan <keeganjoubert@protonmail.com>
2024-03-13 08:11:00 -0700

    101-103

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+-- List all shows by their rating
+SELECT tv_shows.title, SUM(rating) AS rating_sum FROM tv_shows
+JOIN tv_show_rating ON tv_shows.id = tv_show_rating.show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
