d3602fc7a68c72e1fc475bc236e4d91865738f62
Keegan <keeganjoubert@protonmail.com>
2024-03-13 08:14:12 -0700

    102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,6 @@
--- List all shows by their rating
-SELECT tv_shows.title, SUM(rating) AS rating_sum FROM tv_shows
+-- List all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, SUM(tv_show_rating.rating) AS rating_sum
+FROM tv_shows
 JOIN tv_show_rating ON tv_shows.id = tv_show_rating.show_id
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
