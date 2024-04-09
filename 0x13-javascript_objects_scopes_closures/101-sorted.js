59b9473230aa62ca37496d9385107d5d5dee55b2
Keegan <keeganjoubert@protonmail.com>
2024-04-09 16:11:34 -0700

    Update Java

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-
-let result = {};
-Object.keys(dict).forEach(key => {
-  let occurrence = dict[key];
-  if (!result[occurrence]) {
-    result[occurrence] = [];
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-  result[occurrence].push(key);
-});
-
-console.log(result);
+}
+console.log(newDict);
