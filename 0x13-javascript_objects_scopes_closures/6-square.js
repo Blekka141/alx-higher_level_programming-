a679d24aef5a8c0b46bee6ce5dda5dee76b78d9c
Keegan <keeganjoubert@protonmail.com>
2024-04-09 16:06:36 -0700

    update Java

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-const Square = require('./5-square');
-
-class ImprovedSquare extends Square {
-  charPrint(c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
-}
-
-module.exports = ImprovedSquare;
+};
